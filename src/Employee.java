e4fd843df8cfe17a3b6541a51a5ab63744acae3f
AkBePEEK <154293625+AkBePEEK@users.noreply.github.com>
2024-01-16 06:23:52 +0600

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee extends Person {
+    private String position;
+    private double salary;
+
+    @Override
+    public String toString() {
+        return "Employee: " + getId() + ". " + getName() +
+                " " + getSurname();
+    }
+
+    @Override
+    public double getPaymentAmount() {
+        return salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Employee() {
+    }
+
+    public Employee(String name, String surname, String position, double salary) {
+        super(name,surname);
+        this.position = position;
+        this.salary = salary;
+    }
 }
